f29f58fde9d0c0cff2a6dbaf0b28ccc8b903af56
Robert Gordon <gordonrobertd@gmail.com>
2024-09-22 11:03:02 -0700

    Add auth routes

--- controllers/authController.js
+++ controllers/authController.js
@@ -22,7 +22,7 @@ const loginUser = (req, res) => {
 
   console.log('loginController.jsx - loginUser', email, password)
 
-  res.status(200)
+  res.status(200).json( {result: "success"})
 }
 
 module.exports = {
--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const authRoutes = require('./routes/authRoutes')
+
+app.use(express.json())  // required for req.body
+
+app.use('/auth', authRoutes)
+
 // Serve your front end
 app.use("/", express.static("public"));
+
 
 
 app.listen(port, () => {
